54ea3379ae6ae1ffc44c0fb00599bd5068d1eba1
Che <cgoranov@gmail.com>
2022-04-28 20:20:20 -0500

    add before_validations downcase_name to characters class

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,12 @@
 class Character < ApplicationRecord
     validates :name, presence: true
     validates :fun_fact, presence: true
+    before_validation :downcase_name
+
+    private
+
+    def downcase_name
+        self.name = self.name.downcase
+    end
+
 end
