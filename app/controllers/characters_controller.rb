0f31d9d6671e15b320eba6e94b401cc291e76e5d
Che <cgoranov@gmail.com>
2022-04-28 19:44:07 -0500

    add render json for index and create

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,59 +4,61 @@ class CharactersController < ApplicationController
   # GET /characters or /characters.json
   def index
     @characters = Character.all
-
+    render json: @characters
   end
 
   # GET /characters/1 or /characters/1.json
-  def show
-  end
+  # def show
+  # end
 
   # GET /characters/new
-  def new
-    @character = Character.new
-  end
+  # def new
+  #   @character = Character.new
+  # end
 
-  # GET /characters/1/edit
-  def edit
-  end
+  # # GET /characters/1/edit
+  # def edit
+  # end
 
   # POST /characters or /characters.json
   def create
     @character = Character.new(character_params)
 
-    respond_to do |format|
-      if @character.save
-        format.html { redirect_to character_url(@character), notice: "Character was successfully created." }
-        format.json { render :show, status: :created, location: @character }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
-      end
-    end
+    render json: @character
+
+    # respond_to do |format|
+    #   if @character.save
+    #     format.html { redirect_to character_url(@character), notice: "Character was successfully created." }
+    #     format.json { render :show, status: :created, location: @character }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @character.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /characters/1 or /characters/1.json
-  def update
-    respond_to do |format|
-      if @character.update(character_params)
-        format.html { redirect_to character_url(@character), notice: "Character was successfully updated." }
-        format.json { render :show, status: :ok, location: @character }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @character.update(character_params)
+  #       format.html { redirect_to character_url(@character), notice: "Character was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @character }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @character.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /characters/1 or /characters/1.json
-  def destroy
-    @character.destroy
+  # def destroy
+  #   @character.destroy
 
-    respond_to do |format|
-      format.html { redirect_to characters_url, notice: "Character was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to characters_url, notice: "Character was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
