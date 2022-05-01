3f20f3ec09264519d08f0d274ba8b0e937bb29bc
Che <cgoranov@gmail.com>
2022-04-30 19:01:56 -0500

    removed downcase validation

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,10 +3,4 @@ class Character < ApplicationRecord
     validates :fun_fact, presence: true
     before_validation :downcase_name
 
-    private
-
-    def downcase_name
-        self.name = self.name.downcase
-    end
-
 end
