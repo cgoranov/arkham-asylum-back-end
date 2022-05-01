1bfe62722e2a8de1e491bc0a291dbcd2efc8f902
Che <cgoranov@gmail.com>
2022-05-01 10:56:28 -0500

    removed Batman from seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Character.create(name: "Batman", fun_fact: "The Dark Knight")
 Character.create(name: "Joker", fun_fact: "An insanely homiciadal super-villain. Also known as the Clown Prince of Crime and Batman's greatest rival.")
 Character.create(name: "The Riddler", fun_fact: "One of the most intelligent and Batman often works with him on cases... even if its against the Riddler's wishes.")
 Character.create(name: "Harley Quinn", fun_fact: "Also known as Joker's girlfriend, Quinn first appeared in Batman: The Animated Series and was orgininally Joker's Psychiatrist.")
